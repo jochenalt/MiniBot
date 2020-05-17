7829849d8277fcdee31c3f0d2508501f3fb48ea9
Jochen Alt <jochen.alt@gmx.net>
2020-05-18 00:32:48 +0200

    check if roscore is running

--- code/ros/minibot/bin/core.sh
+++ code/ros/minibot/bin/core.sh
@@ -1,9 +1,30 @@
-roscore &
+#!/bin/bash
 
-# before we can call  rosparam, roscore neds to be up
-sleep 5
+rosservice find /rosout
+status=$$
+if [ $status -ne 0 ]; then
+   echo "roscore is running already"
+else
+   echo "starting roscore"
+   roscore &
+   sleep 1
+   rosservice find /rosout
+   while [ $$ -ne 0 ]
+   do
+      echo "not yet up"
+      sleep 1
+      rosservice find /rosout
+   done
+   echo "roscore up and running"
+fi
+
+echo "setting localhost:62345 for mongodb"
 rosparam set mongodb_port 62345
 rosparam set mongodb_host localhost
 
 # start mongo db
+echo "starting mongodb"
 rosrun mongodb_store mongodb_server.py &
+echo "done"
+
+sleep 5
