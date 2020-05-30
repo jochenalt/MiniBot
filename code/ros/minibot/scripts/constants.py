25429010cbd4508514982e1458586a5275942f97
Jochen Alt <jochen.alt@gmx.net>
2020-05-30 16:02:44 +0200

    Revert "Started descartes"
    This reverts commit 2a9df7577e22689348ea654214f7afb2c80cd927.

--- code/ros/minibot/scripts/constants.py
+++ code/ros/minibot/scripts/constants.py
@@ -5,5 +5,5 @@ class Constants:
 		ORIENTATION_RESOLUTION = 0.00001 	# [rad]
 		ARM_GROUP = 'minibot_arm'  			# group of links as defined in SRDF defining the arm without the endeffector
 		GRIPPER_GROUP = 'minibot_gripper'  	# group of links of the gripper
-		MINIBOT_GROUP = 'minibot'  			# group of links as defined in SRDF defining the arm with the endeffector
+		MINIBOT_GROUP = 'minibot'  			# group of links as defined in SRDF defining the arm without the endeffector
 		FIXED_FRAME = 'base_link'  			# first link 
